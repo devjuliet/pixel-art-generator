713242d2480506e8ea436665a7331ceed8b353b3
Julieta <juliiy98@gmail.com>
2020-04-08 21:59:48 -0600

    Added primefaces dependency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
+import ToolsBoard from "./components/tools-board/tools-board.component";
+import Menu from "./components/menu/menu.component";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <ToolsBoard />
+      <Menu />
+    </div>
+  );
 }
 
 export default App;
