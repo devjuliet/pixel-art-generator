20f8046fdb2f0f310153b2ae3c27509aaee55bd9
Julieta <juliiy98@gmail.com>
2020-04-14 21:33:11 -0600

    Added table rows and columns

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./Canvas.css";
 
 class Canvas extends Component {
     constructor() {
         super();
         this.state = {
-            height: 30,
+            height: 15,
             width: 30,
         };
     }
@@ -13,19 +14,25 @@ class Canvas extends Component {
         this.setState({ value: event.target.value });
     }
 
-    componentDidMount(){
+    createTable = () => {
+        let table = [];
 
-
-    }
+        // this need to be handle by a component
+        for (let i = 0; i < this.state.height; i++) { 
+            let children = [];
+            for (let j = 0; j < this.state.width; j++) {
+                children.push(<td></td>); // width
+            }
+            //this too
+            table.push(<tr>{children}</tr>); // height
+        }
+        return table;
+    };
 
     render() {
         return (
-            <div>
-                <table class="table table-bordered">
-                    <tbody>
-                
-                    </tbody>
-                </table>
+            <div  className="center">
+                <table >{this.createTable()}</table>
             </div>
         );
     }
